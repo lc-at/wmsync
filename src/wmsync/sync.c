02c4f9b5789eff70bd07541849d700a1c64eac9f
loncat <30001379+p4kl0nc4t@users.noreply.github.com>
2021-10-02 00:14:42 +0800

    [unfinished] Trying to implement Windows APIs

--- src/wmsync/sync.c
+++ src/wmsync/sync.c
@@ -1,18 +1,29 @@
-#include "common.h"
-#include <netdb.h>
+#define WIN32_LEAN_AND_MEAN
 
-bool check_connection();
+#include "common.h"
+#include <winsock2.h>
+#include <windows.h> // noqa
+#include <iphlpapi.h>
+
+#define WORKING_BUFFER_SIZE 15000
+
+#pragma comment(lib, "IPHLPAPI.lib")
+#pragma comment(lib, "Ws2_32.lib")
+
+long get_ip_address();
 
 int start_sync(configuration *p_config) {
-    log_info("initializing synchronizer");
-    log_info("checking connection");
-    check_connection();
+    log_info("Initializing synchronizer");
+    log_info("Checking connection");
+    get_ip_address();
     return 0;
 }
 
-bool check_connection() {
-    struct hostent *h;
-    h = gethostbyname("google.com");
-    log_trace("%d", h->h_length);
+long get_ip_address() {
+    IPAddr dest_addr = inet_addr("8.8.8.8");
+    DWORD best_if_index;
+    DWORD retval = GetBestInterface(dest_addr, &best_if_index);
+    log_trace("%d", retval);
+    log_trace("%d", best_if_index);
     return 0;
 }
