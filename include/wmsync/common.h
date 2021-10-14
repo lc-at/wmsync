9b7059e8514345dbe2250c254f11c78b348c777f
loncat <30001379+p4kl0nc4t@users.noreply.github.com>
2021-10-15 00:54:06 +0800

    Defined some constants

--- include/wmsync/common.h
+++ include/wmsync/common.h
@@ -1,6 +1,10 @@
 #ifndef WMSYNC__COMMON_H
 #define WMSYNC__COMMON_H
 
+#define LOGIN_CHECK_INTERVAL 30 * 1000 // milliseconds
+#define LOGIN_ERROR_TA_INTERVAL 5 * 1000 // milliseconds
+#define IP_ADDR_BUF_SZ 16
+
 #include <log.c/log.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,12 +12,11 @@
 #include <string.h>
 
 typedef struct {
-    int version;
     const char *username;
     const char *password;
     const char *gw_id;
     const char *wlan;
-    uint8_t mac[6];
+    const char *mac;
 } configuration;
 
 int start_sync(configuration *config);
